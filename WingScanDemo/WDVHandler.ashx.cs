9b717b5e3f3883dac57094df582e7e4ad28c3122
Atalasoft Support <developersupport@atalasoft.com>
2026-04-08 14:11:08 -0400

    initial commit

---
+++ WingScanDemo/WDVHandler.ashx.cs
@@ -0,0 +1,14 @@
+﻿using Atalasoft.Imaging.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class WDVHandler : Atalasoft.Imaging.WebControls.WebDocumentRequestHandler
+{
+    public WDVHandler()
+    {
+        /// IMPORTANT!: This is added for security purposes - it fully disables file upload
+        this.FileUpload += new FileUploadEventHandler(delegate(object sender, FileUploadEventArgs e) { e.Cancel = true; e.Overwrite = false; });
+    }
+}
